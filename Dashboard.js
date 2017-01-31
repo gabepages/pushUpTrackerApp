4f6a1c37726ed896a9dce3bd68053633084ba282
Gabriel Pages <gbpages@yahoo.com>
2017-01-31 00:02:36 -0500

    getting error: RCTJSONStringify() encountered the following error: Invalid number value (infinite) in JSON write

--- Dashboard.js
+++ Dashboard.js
@@ -14,39 +14,62 @@ import {
   Platform,
   TextInput,
   PixelRatio,
+  TouchableOpacity,
+  Image,
+  AlertIOS,
   Button
 } from 'react-native';
 
+import SideMenu from 'react-native-side-menu'
 
 
-export default class Dashboard extends Component {
-
+class Menu extends Component {
   render() {
-    console.log(this.state, this.props);
     return (
-      <View style={{flex:1}}>
-        <View style={styles.header}>
-          <Button
-            onPress={() => console.log('Hello Wolrd')}
-            title="Menu"
-            color="#fff"
-          />
-          <Text style={{textAlign: "center"}}>Push Up Tracker</Text>
-        </View>
-        <View>
-          <Text style={{textAlign: "center"}}>Hello World</Text>
-        </View>
+      <View>
+        <Text>menu 1</Text>
+        <Text>menu 2</Text>
       </View>
     );
   }
 }
+
+const menu = <Menu />
+
+export default class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <SideMenu menu={menu}>
+        <View>
+          <View>
+            <TouchableOpacity onPress={this.handleMenu}>
+              <Image source={require('./menu.png')}
+                     style={{width: 25, height: 25, margin: 10}}  />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </SideMenu>
+    );
+  }
+  handleMenu () {
+    AlertIOS.alert(
+     'Sync Complete',
+     'All your data are belong to us.'
+    );
+  }
+}
+
+
+
 
 
 const styles = StyleSheet.create({
   header: {
     flexDirection: "row",
     alignItems: "center",
-    justifyContent: "space-between",
     backgroundColor:"#0097a7",
     height:45,
   }
